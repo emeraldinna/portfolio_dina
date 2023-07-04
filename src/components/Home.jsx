d0faab830ad6fffce7822af5d5a27b388ddb7516
Inna Dyachkova <inna.dyachkova@gmail.com>
2023-07-04 13:56:50 +0200

    Added styles and settings to improve the appearance of the grid:
    - Set a fixed height for images in the grid
    - Applied overflow and positioning to handle image proportions
    - Adjusted object-fit and positioning for proper image display

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,23 +7,42 @@ import data from '../data/mainProjects.json';
 const Home = () => {
   return (
     <Container>
-      <h1 className="mb-5 text-uppercase" style={{ fontFamily: 'Oswald-SemiBold' }}>Portfolio</h1>
-      <Row className="mb-2">
-        {data.map(project => (
-          <Col key={project.id} xs={12} md={6} xl={4} className="mx-auto px-1 py-1">
-            <Image src={`/images/home-page/${project.source}`} />
-          </Col>
-        ))}
-      </Row>
-    </Container>
+    <h1 className="mb-5 text-uppercase" style={{ fontFamily: 'Oswald-SemiBold' }}>Portfolio</h1>
+    <Row className="mb-4">
+      {data.map(project => (
+        <Col key={project.id} xs={12} md={6} lg={4} className="px-1 py-1">
+          <div style={{ height: '300px', overflow: 'hidden', position: 'relative' }}>
+            <Image
+              src={`/images/home-page/${project.source}`}
+              fluid
+              style={{
+                width: '100%',
+                height: '100%',
+                objectFit: 'cover',
+                position: 'absolute',
+                top: '0',
+                left: '0',
+              }}
+            />
+          </div>
+        </Col>
+      ))}
+    </Row>
+  </Container>
   )
 }
 
 export default Home;
 
-// <Col xs={12} md={6} xl={4} className="mx-auto px-1 py-1">
-// <Image src="/images/home-page/02.jpg" />
-// </Col>
-// <Col xs={12} md={6} xl={4} className="mx-auto px-1 py-1">
-// <Image src="/images/home-page/05.jpg" />
-// </Col>
+// <Container>
+// <h1 className="mb-5 text-uppercase" style={{ fontFamily: 'Oswald-SemiBold' }}>Portfolio</h1>
+// <Row className="mb-2">
+//   {data.map(project => (
+//     <Col key={project.id} xs={12} md={6} xl={4} className="mx-auto px-1 py-1">
+//       <div className="image-container">
+//         <Image src={`/images/home-page/${project.source}`} fluid />
+//       </div>
+//     </Col>
+//   ))}
+// </Row>
+// </Container>
