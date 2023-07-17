4d2e20bde1eaed48cc708cc97786d9c6fe9c8c94
Inna Dyachkova <inna.dyachkova@gmail.com>
2023-07-17 20:42:23 +0200

    Create animation.json file and display projects using the JSON data on general animation page

--- src/components/Animation.jsx
+++ src/components/Animation.jsx
@@ -2,6 +2,8 @@ import { Fragment } from 'react';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import data from '../data/animations.json';
 
 const Animation = () => {
   
@@ -12,27 +14,28 @@ const Animation = () => {
         <Image className="hero-image" src="/baner-animation-test.jpg" fluid />
       </div>
       <Container className="mt-5">
-        <Row>
-          <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut mi in nisl faucibus tempor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris commodo mauris sapien, sed ultrices arcu mattis in. Nullam sodales augue dolor, vel pretium nisl gravida et. Cras vel molestie ipsum. Sed nec sem erat. Sed vitae fermentum dolor. Mauris venenatis tellus a volutpat accumsan. Quisque volutpat fringilla libero fringilla pellentesque. Nunc nec suscipit ligula. Ut quis nisi sollicitudin, auctor ligula a, aliquet elit. Suspendisse quam lectus, condimentum ac arcu vel, accumsan pretium justo. Duis aliquet velit mauris. Donec finibus, dui sit amet elementum cursus, nunc ligula dictum libero, sit amet gravida nisi eros non lacus. Cras dui tortor, laoreet eget ex vel, malesuada fringilla urna.
-
-            Morbi sed tortor vel ligula ultrices dictum. In condimentum porta ipsum et faucibus. Quisque a porta turpis. Vivamus ac nunc sit amet nisl ultricies placerat. Suspendisse eros velit, aliquet sit amet turpis eget, porta fringilla orci. Suspendisse enim sem, convallis quis euismod ut, vestibulum a leo. Cras laoreet felis erat, vel laoreet sem gravida eget. Nulla eu urna libero. Phasellus sodales ac neque at placerat.
-
-            Vestibulum eros velit, venenatis quis purus a, consequat lacinia elit. Donec egestas imperdiet justo, a fringilla neque tincidunt at. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse faucibus diam sit amet odio faucibus, eget venenatis risus congue. Morbi ultrices sed dolor id accumsan. Aliquam malesuada dictum odio, non maximus arcu egestas id. Donec gravida odio turpis, a convallis urna porttitor sed. Cras varius, neque eget dapibus dapibus, lorem erat scelerisque nisi, nec elementum elit ligula at lacus. Suspendisse rhoncus faucibus risus id viverra. Aenean tincidunt neque elementum ipsum pretium hendrerit. Cras eget ex a ipsum tincidunt tincidunt. Donec sollicitudin ipsum eget nulla luctus, eget sollicitudin ligula cursus. In vulputate neque sit amet dui euismod faucibus. Suspendisse vestibulum imperdiet velit, eu luctus enim. Quisque auctor sit amet diam nec lacinia.
-
-            Aenean commodo, odio eu auctor efficitur, risus orci pharetra ipsum, vitae tempus mauris sem ultricies turpis. Donec sed metus massa. Suspendisse vehicula leo eget neque laoreet viverra. Nullam sagittis lacinia tincidunt. Nunc condimentum luctus ipsum venenatis mollis. Donec rhoncus lacus felis, suscipit vestibulum tortor egestas at. Donec non dui nisi. Vivamus rutrum quis tortor sed vulputate. Vestibulum dui nisl, molestie ac est eu, mollis facilisis augue.
-
-            Sed suscipit quam lacinia, sollicitudin orci eu, scelerisque diam. Mauris eu lobortis est. Nulla dapibus nulla vel nisi consequat, eget ultricies elit pellentesque. Donec mollis tellus eu enim volutpat laoreet. Nulla ornare lectus justo, eget gravida ligula pulvinar et. Vestibulum lacinia at enim vel convallis. Ut feugiat, erat eu aliquam accumsan, neque erat tincidunt tellus, ut dignissim ipsum magna ac ipsum. Pellentesque justo est, volutpat id dui ac, cursus laoreet nisi. Fusce sodales, eros ut tempor aliquet, nisi justo gravida nibh, in interdum nisi diam a risus. Aliquam id laoreet arcu, in luctus mauris. Vestibulum consequat urna ligula, a porta risus tincidunt eu. Curabitur nibh odio, dignissim ut ipsum vel, eleifend aliquam eros. Nam nisl metus, vestibulum molestie erat id, semper commodo arcu. Nunc vulputate lorem et rhoncus iaculis. Duis sit amet ligula tellus. Suspendisse nec dictum dui.</div>
-          <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut mi in nisl faucibus tempor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris commodo mauris sapien, sed ultrices arcu mattis in. Nullam sodales augue dolor, vel pretium nisl gravida et. Cras vel molestie ipsum. Sed nec sem erat. Sed vitae fermentum dolor. Mauris venenatis tellus a volutpat accumsan. Quisque volutpat fringilla libero fringilla pellentesque. Nunc nec suscipit ligula. Ut quis nisi sollicitudin, auctor ligula a, aliquet elit. Suspendisse quam lectus, condimentum ac arcu vel, accumsan pretium justo. Duis aliquet velit mauris. Donec finibus, dui sit amet elementum cursus, nunc ligula dictum libero, sit amet gravida nisi eros non lacus. Cras dui tortor, laoreet eget ex vel, malesuada fringilla urna.
-
-            Morbi sed tortor vel ligula ultrices dictum. In condimentum porta ipsum et faucibus. Quisque a porta turpis. Vivamus ac nunc sit amet nisl ultricies placerat. Suspendisse eros velit, aliquet sit amet turpis eget, porta fringilla orci. Suspendisse enim sem, convallis quis euismod ut, vestibulum a leo. Cras laoreet felis erat, vel laoreet sem gravida eget. Nulla eu urna libero. Phasellus sodales ac neque at placerat.
-
-            Vestibulum eros velit, venenatis quis purus a, consequat lacinia elit. Donec egestas imperdiet justo, a fringilla neque tincidunt at. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse faucibus diam sit amet odio faucibus, eget venenatis risus congue. Morbi ultrices sed dolor id accumsan. Aliquam malesuada dictum odio, non maximus arcu egestas id. Donec gravida odio turpis, a convallis urna porttitor sed. Cras varius, neque eget dapibus dapibus, lorem erat scelerisque nisi, nec elementum elit ligula at lacus. Suspendisse rhoncus faucibus risus id viverra. Aenean tincidunt neque elementum ipsum pretium hendrerit. Cras eget ex a ipsum tincidunt tincidunt. Donec sollicitudin ipsum eget nulla luctus, eget sollicitudin ligula cursus. In vulputate neque sit amet dui euismod faucibus. Suspendisse vestibulum imperdiet velit, eu luctus enim. Quisque auctor sit amet diam nec lacinia.
-
-            Aenean commodo, odio eu auctor efficitur, risus orci pharetra ipsum, vitae tempus mauris sem ultricies turpis. Donec sed metus massa. Suspendisse vehicula leo eget neque laoreet viverra. Nullam sagittis lacinia tincidunt. Nunc condimentum luctus ipsum venenatis mollis. Donec rhoncus lacus felis, suscipit vestibulum tortor egestas at. Donec non dui nisi. Vivamus rutrum quis tortor sed vulputate. Vestibulum dui nisl, molestie ac est eu, mollis facilisis augue.
-
-            Sed suscipit quam lacinia, sollicitudin orci eu, scelerisque diam. Mauris eu lobortis est. Nulla dapibus nulla vel nisi consequat, eget ultricies elit pellentesque. Donec mollis tellus eu enim volutpat laoreet. Nulla ornare lectus justo, eget gravida ligula pulvinar et. Vestibulum lacinia at enim vel convallis. Ut feugiat, erat eu aliquam accumsan, neque erat tincidunt tellus, ut dignissim ipsum magna ac ipsum. Pellentesque justo est, volutpat id dui ac, cursus laoreet nisi. Fusce sodales, eros ut tempor aliquet, nisi justo gravida nibh, in interdum nisi diam a risus. Aliquam id laoreet arcu, in luctus mauris. Vestibulum consequat urna ligula, a porta risus tincidunt eu. Curabitur nibh odio, dignissim ut ipsum vel, eleifend aliquam eros. Nam nisl metus, vestibulum molestie erat id, semper commodo arcu. Nunc vulputate lorem et rhoncus iaculis. Duis sit amet ligula tellus. Suspendisse nec dictum dui.
-          </div>
+        <Row className="mb-4">
+          {data.map(project => (
+            <Col key={project.id} xs={12} md={6} lg={4} className="px-1 py-1">
+              <div
+                style={{ height: '300px', overflow: 'hidden', position: 'relative' }}
+              >
+                <Image
+                  src={`/images/home-page/${project.source}`}
+                  fluid
+                  style={{
+                    width: '100%',
+                    height: '100%',
+                    objectFit: 'cover',
+                    position: 'absolute',
+                    top: '0',
+                    left: '0',
+                    cursor: 'pointer',
+                  }}
+                />
+              </div>
+            </Col>
+          ))}
         </Row>
       </Container>
     </Fragment>
