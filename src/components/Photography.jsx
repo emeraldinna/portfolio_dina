455e044020560d737f31d26c1cb53faf7628ba1d
Inna Dyachkova <inna.dyachkova@gmail.com>
2023-07-28 22:29:32 +0200

    Add hero banner to general photography page

--- src/components/Photography.jsx
+++ src/components/Photography.jsx
@@ -1,11 +1,24 @@
+import { Fragment } from 'react';
 import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Photography = () => {
   return (
-    <Container className="mt-5">
-        <h1 className="text-uppercase" style={{ fontFamily: 'Oswald-SemiBold' }}>Photography</h1>
-    </Container>
-  )
+    <Fragment>
+      <div className="hero-wrapper">
+        <div className="hero-overlay" />
+        <Image className="hero-image" src="/baner-photography-test.jpg" fluid />
+      </div>
+      <Container className="mt-5">
+        <Row className="mb-4 justify-content-center">
+          <Col xs={11} sm={12} md={8} lg={6} xl={6} xxl={4} className="px-1 py-1 justify-content-center">
+          </Col>
+        </Row>
+      </Container>
+    </Fragment>
+  );
 }
 
 export default Photography;
