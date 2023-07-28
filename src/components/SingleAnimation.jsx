447f03107a7b4d490c065b101cd4942a74e6def1
Inna Dyachkova <inna.dyachkova@gmail.com>
2023-07-28 18:04:12 +0200

    feat: add button "Go back" on Single Animation page, upon click user returns to the General Animation page

--- src/components/SingleAnimation.jsx
+++ src/components/SingleAnimation.jsx
@@ -1,14 +1,20 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ReactPlayer from 'react-player';
+import Button from 'react-bootstrap/Button';
 
 const SingleAnimation = () => {
     const location = useLocation();
     const { project } = location.state;
     // console.log(project);
     const markup = { __html: project.description };
+    const navigate = useNavigate();
+
+    const handleGoBack = () => {
+        navigate(-1);
+    }
 
     return (
         <Container className="mt-5">
@@ -16,7 +22,7 @@ const SingleAnimation = () => {
                 {project.videos.map((video, index) => (
                     <Col key={index} xs={12} sm={12} md={10} lg={8} xl={6} xxl={6} className="py-4">
                         <div
-                            style={{ position: 'relative', width: '100%', height: '300px' }} 
+                            style={{ position: 'relative', width: '100%', height: '300px' }}
                         >
                             <ReactPlayer
                                 url={video.url}
@@ -34,9 +40,10 @@ const SingleAnimation = () => {
                 ))}
             </Row>
             <Row className="mb-4 justify-content-center" style={{ textAlign: 'left' }}>
-                    <div><h2 className="fw-bold" style={{ fontFamily: 'Oswald-SemiBold' }}>{project.title}</h2></div>
-                    <div dangerouslySetInnerHTML={markup}  className="mb-4"/>
-                    {project.link && <a href={project.link} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}><p>Official YouTube channel</p></a>}
+                <div><h2 className="fw-bold" style={{ fontFamily: 'Oswald-SemiBold' }}>{project.title}</h2></div>
+                <div dangerouslySetInnerHTML={markup} className="mb-4" />
+                {project.link && <a href={project.link} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}><p>Official YouTube channel</p></a>}
+                <Button variant="link" style={{ color: 'black', textAlign: 'left' }} onClick={handleGoBack}>Go back</Button>
             </Row>
         </Container>
     );
