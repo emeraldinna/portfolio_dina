8a1714fc2ec7becf73665ad3aca9fae48c94dfaa
Inna Dyachkova <inna.dyachkova@gmail.com>
2024-02-18 11:42:15 +0100

    fix: implemented scroll to top on page change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ReactGA from 'react-ga4';
+import ScrollToTop from './components/ScrollToTop';
 import MainLayout from './layouts/MainLayout';
 import Home from './components/Home';
 import './App.css';
@@ -19,6 +20,7 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<MainLayout />}>
             <Route index element={<Home />} />
