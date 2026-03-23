488fc3a210ec8ab2f61faf07e8ebd187ad4ba85d
Boris Berkgaut <berkgaut@gmail.com>
2026-03-23 13:05:44 +0100

    migrate Home -> tsx

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,10 +1,11 @@
 import { z } from "zod"
 import photos from "./photos.json"
+import mainPhotos from "./mainProjects.json"
 
 const RelatedItemSchema = z.object({
     smallSource: z.string(),
     largeSource: z.string(),
-})
+});
 
 const PhotoItemSchema = z.object({
     id: z.union([z.number(), z.string()]),
@@ -22,11 +23,24 @@ const PhotoItemSchema = z.object({
     link: z.url().optional(),
     linkDescription: z.string().optional(),
     banner: z.string().optional(),
-})
+});
 
-export type PhotoItem = z.infer<typeof PhotoItemSchema>
+export type PhotoItem = z.infer<typeof PhotoItemSchema>;
 
-export const PhotosSchema = z.array(PhotoItemSchema)
+export const PhotosSchema = z.array(PhotoItemSchema);
+
+// pretty close to PhotosItem, but not exactly. I'll refactor this later
+export const MainPhotoItemSchema = z.object({
+    id: z.union([z.number(), z.string()]),
+    alt: z.string(),
+    title: z.string(),
+    smallSource: z.string(),
+    largeSource: z.string(),
+    type: z.literal("photo"),
+});
+export type MainPhotoItem = z.infer<typeof MainPhotoItemSchema>;
+
+export const MainPhotosSchema = z.array(MainPhotoItemSchema);
 
 export function getPhotos(): { success: true, photos: PhotoItem[]} | { success: false, message: string } {
     const parseResult = PhotosSchema.safeParse(photos);
@@ -34,4 +48,12 @@ export function getPhotos(): { success: true, photos: PhotoItem[]} | { success: 
         return { success: true, photos: parseResult.data };
     }
     return { success: false, message: parseResult.error.message}
+}
+
+export function getMainPhotos(): { success: true, photos: MainPhotoItem[] } | { success: false, message: string }{
+    const parseResult = MainPhotosSchema.safeParse(mainPhotos);
+    if (parseResult.success) {
+        return { success: true, photos: parseResult.data };
+    }
+    return { success: false, message: parseResult.error.message};
 }
